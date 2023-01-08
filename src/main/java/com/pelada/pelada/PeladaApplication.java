9078cdbc12e1af3db247c267cfd5560bdff421ec
fernandobituco <fernandoangelima@gmail.com>
2023-01-08 16:10:12 -0300

    Base project created

---
+++ src/main/java/com/pelada/pelada/PeladaApplication.java
@@ -0,0 +1,13 @@
+package com.pelada.pelada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeladaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeladaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelada/pelada/PeladaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelada.pelada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeladaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
